b899535895007f72fa9276ea4a3f2cd68e9842cf
Victoria Fajardo <victoria.fajardo@gmail.com>
2016-08-30 17:44:26 -0500

    append and style

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
+$(document).ready(function() {
 
-
-for (i = 1; i <= 100; i++) {
-  var fizzbuzz = "";
- if (i % 3 == 0) fizzbuzz += "Fizz";
- if (i % 5 == 0) fizzbuzz += "Buzz";
-console.log(fizzbuzz || i);
-}
+  for (i = 1; i <= 100; i++) {
+    var fizzbuzz = "";
+    var result = "";
+   if (i % 3 == 0) fizzbuzz += "Fizz";
+   if (i % 5 == 0) fizzbuzz += "Buzz";
+  // console.log(fizzbuzz || i);
+  result = fizzbuzz || i;
+  $(".fizz_buzz").append(result + "<br>");
+  // $(".fizz_buzz").append((fizzbuzz || i) + "<br>");
+  }
+});
